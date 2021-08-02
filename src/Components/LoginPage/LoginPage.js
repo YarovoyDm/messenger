901b38bc1fa1ccc0510a2881c6b0b283c8018241
Mac <mac@Macs-MacBook-Pro.local>
2021-08-02 20:38:42 +0300

    add title

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -7,7 +7,7 @@ export function LoginPage(){
     return <div className={styles.loginPage}>
         <div className={styles.content}>
             <TelegrtamLogo className={styles.telegramLogo}/>
-            <div className={styles.title}></div>
+            <div className={styles.title}>Telegram</div>
         </div>
     </div>
 }
