89167356e7bd7530fa284c9415c9a562c1e2fba6
Dmitriy Poplinski <dmytro.poplinski@gmail.com>
2021-08-03 22:03:29 +0300

    changed App component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import './App.css';
 import { LoginPage } from './Components/LoginPage/LoginPage';
+import { DialogsPage } from './Components/DialogsPage/DialogsPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path='/login' component={LoginPage} />
+        <Route path='/dialogs' component={DialogsPage} />
       </Switch>
     </div>
   );
---
+++ src/Components/DialogsPage/DialogsPage.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {ReactComponent as TelegrtamLogo} from '../../images/telegramLogo.svg'
+
+import styles from './DialogsPage.module.css'
+
+export function DialogsPage(){
+    return <div >
+        Hello world
+    </div>
+}
